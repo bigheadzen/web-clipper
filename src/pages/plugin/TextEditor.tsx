615a27105b4dd99d8df72132a799b851352bfa1b
BigHeadZen <guojun.tech@gmail.com>
2020-10-16 00:12:15 +0800

    Fix relative image path issue.

--- src/pages/plugin/TextEditor.tsx
+++ src/pages/plugin/TextEditor.tsx
@@ -33,10 +33,47 @@ const editorId = 'DiamondYuan_Love_LJ';
 
 class ClipperPluginPage extends React.Component<PageProps> {
   private myCodeMirror: any;
+  private pageUrl: URL | undefined;
 
   checkExtension = () => {
     const { extension, clipperData, pathname } = this.props;
-    const data = clipperData[pathname];
+    var data = clipperData[pathname];
+
+    if (this.pageUrl === undefined) {
+      throw new Error("pageUrl not inited correctly.");
+    }
+
+    if (typeof(data) === "string") {
+      const result = data.match(/!\[.*?\]\((.*?)\)/g);
+      if (result) {
+        const images: string[] = result
+          .map(o => {
+            const temp = /!\[.*?\]\((.*?)\)/.exec(o);
+            if (temp) {
+              return temp[1];
+            }
+            return '';
+          })
+          .filter(o => o.length > 0)
+
+          for (let image of images) {
+            if (image.startsWith("http://") || image.startsWith("https://")) {
+              continue;
+            }
+            var folder = "/";
+            var path = this.pageUrl.pathname;
+            if (path.endsWith("/")) {
+              folder = path;
+            } else {
+              const pos = path.lastIndexOf("/");
+              folder = path.substring(0, pos + 1);
+            }
+            const imageUrl = `${this.pageUrl.protocol}//${this.pageUrl.host}${folder}${image}`
+            data = data.replace(image, imageUrl)
+          }
+        }
+    }
+
     if (isUndefined(data)) {
       this.props.asyncRunExtension({
         pathname,
@@ -63,31 +100,52 @@ class ClipperPluginPage extends React.Component<PageProps> {
     }
   };
 
-  componentDidMount = () => {
-    const data = this.checkExtension();
-    let myTextarea = document.getElementById(editorId) as HTMLTextAreaElement;
-    this.myCodeMirror = HyperMD.fromTextArea(myTextarea, {
-      lineNumbers: false,
-      hmdModeLoader: false,
-    });
-    if (this.myCodeMirror) {
-      const value = this.myCodeMirror.getValue();
-      if (data !== value) {
-        this.myCodeMirror.setValue(data);
+  componentDidMountInner = (cb: (pageUrl: string) => void) => {
+    chrome.tabs.query(
+      {
+        active: true,
+        currentWindow: true,
+      },
+      function(tabs) {
+        const url = tabs[0].url === undefined ? "" : tabs[0].url
+        console.log(`Got page URL: ${url}`)
+        cb(url)
       }
-    }
-    this.myCodeMirror.on('change', (editor: any) => {
-      this.props.changeData({
-        data: editor.getValue(),
-        pathName: this.props.pathname,
+    );
+  }
+
+  componentDidMount = () => {
+    this.componentDidMountInner((url: string) => {
+      if (url.length <= 0) {
+        throw new Error("Failed to get current tab URL. Please donot switch tab.");
+      }
+      this.pageUrl = new URL(url)
+
+      const data = this.checkExtension();
+      let myTextarea = document.getElementById(editorId) as HTMLTextAreaElement;
+      this.myCodeMirror = HyperMD.fromTextArea(myTextarea, {
+        lineNumbers: false,
+        hmdModeLoader: false,
       });
+      if (this.myCodeMirror) {
+        const value = this.myCodeMirror.getValue();
+        if (data !== value) {
+          this.myCodeMirror.setValue(data);
+        }
+      }
+      this.myCodeMirror.on('change', (editor: any) => {
+        this.props.changeData({
+          data: editor.getValue(),
+          pathName: this.props.pathname,
+        });
+      });
+      this.myCodeMirror.setSize(800, 621);
+      if (this.props.liveRendering) {
+        HyperMD.switchToHyperMD(this.myCodeMirror);
+      } else {
+        HyperMD.switchToNormal(this.myCodeMirror);
+      }
     });
-    this.myCodeMirror.setSize(800, 621);
-    if (this.props.liveRendering) {
-      HyperMD.switchToHyperMD(this.myCodeMirror);
-    } else {
-      HyperMD.switchToNormal(this.myCodeMirror);
-    }
   };
 
   render() {
